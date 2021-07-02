742d3edea6d553a66754e403acc0ce3dc059f954
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-07-02 21:02:59 +0900

    init

---
+++ SwiftUIKitTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftUIKitTableView
+//
+//  Created by 宮本光直 on 2021/07/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
